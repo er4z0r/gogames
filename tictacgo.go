a3553eca1d773e7a932a4d166e2c0eac6bd7bf85
Tilman Bender <mail@er4z0r.com>
2020-03-10 21:15:51 +0100

    Moved the structs and interface to a new package

--- tictacgo.go
+++ tictacgo.go
@@ -2,10 +2,36 @@ package main
 
 import (
 	"fmt"
-	"er4z0r/games"
+
+	. "github.com/er4z0r/games"
 )
 
 func main() {
-	Board b := new(Board)
 	fmt.Println("Hello Tic Tac Go!")
+
+	var b Board
+	b = new(Simple2DBoard)
+
+	//Prompt for player names
+
+	//Prompt for board size
+	b.Init(3, 3)
+
+	//While the game is not over
+	//Prompt Player for coordinates
+	//Identify the winner
+
+	b.Set(Position{X: 0, Y: 0}, "o")
+	b.Set(Position{X: 0, Y: 1}, "x")
+	b.Set(Position{X: 0, Y: 2}, "x")
+
+	b.Set(Position{X: 1, Y: 0}, "x")
+	b.Set(Position{X: 1, Y: 1}, "o")
+	b.Set(Position{X: 1, Y: 2}, "x")
+
+	b.Set(Position{X: 2, Y: 0}, "x")
+	b.Set(Position{X: 2, Y: 1}, "x")
+	b.Set(Position{X: 2, Y: 2}, "o")
+
+	fmt.Printf("%v\n", b)
 }
