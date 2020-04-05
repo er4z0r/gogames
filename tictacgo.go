a096ec5df276d874ad9bf9fca650bc4b0419f727
Tilman Bender <mail@er4z0r.com>
2020-04-05 14:03:54 +0200

    Finally a primitively working version!

--- tictacgo.go
+++ tictacgo.go
@@ -6,32 +6,56 @@ import (
 	. "github.com/er4z0r/tictacgo/games"
 )
 
+func clear() {
+	fmt.Print("\033[H\033[2J")
+}
+
 func main() {
 	fmt.Println("Hello Tic Tac Go!")
+	var x, y int
 
-	var b Board
-	b = new(Simple2DBoard)
+	b, _ := NewSimple2DBoard(3, 3)
 
 	//Prompt for player names
+	p1 := new(Player)
+	p2 := new(Player)
 
-	//Prompt for board size
-	b.Init(3, 3)
+	fmt.Println("Player1 Name:")
+	fmt.Scan(&p1.Name)
+	p1.Symbol = "o"
 
-	//While the game is not over
-	//Prompt Player for coordinates
+	fmt.Println("Player2 Name:")
+	fmt.Scan(&p2.Name)
+	p2.Symbol = "x"
+
+	var g GameLogic
+	g, _ = NewBaseLogic(b, p1, p2)
+	for {
+		clear()
+		fmt.Printf("\n%v\n", b)
+
+		//While the game is not over
+		if g.IsOver() {
+			break
+		}
+		p := g.WhoseTurn()
+		//Prompt Player for coordinates
+		fmt.Printf("%s's turn. Please enter coordinates:", p.Name)
+		fmt.Scanln(&x, &y)
+
+		if g.IsLegal(Place, p, x, y) {
+			b.Set(x, y, p.Symbol)
+		}
+	}
+
 	//Identify the winner
+	clear()
+	fmt.Println("\t== GAME OVER! ==")
+	fmt.Printf("\n%v\n", b)
+	if w := g.GetWinner(); w != nil {
+		fmt.Printf("Player %s wins!\n", w.Name)
+	} else {
+		fmt.Println("It is a draw. Nobody wins!")
+	}
 
-	b.Set(0, 0, "o")
-	b.Set(0, 1, "x")
-	b.Set(0, 2, "x")
-
-	b.Set(1, 0, "x")
-	b.Set(1, 1, "o")
-	b.Set(1, 2, "x")
-
-	b.Set(2, 0, "x")
-	b.Set(2, 1, "x")
-	b.Set(2, 2, "o")
-
-	fmt.Printf("%v\n", b)
 }
